22c61f6360b847742aee1da04ae415b0bd9727ce
scooter7 <scooter.vineburgh@gmail.com>
2024-04-04 15:01:11 -0500

    Update pv.py

--- pv.py
+++ pv.py
@@ -2,43 +2,35 @@ import streamlit as st
 import pandas as pd
 from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 import os
+import tempfile
 
 st.title("Video Personalization App")
 
-# File uploaders
 video_file = st.file_uploader("Choose a video file", type=["mp4"])
 csv_file = st.file_uploader("Choose a CSV file")
 
 if video_file is not None and csv_file is not None:
-    # Read CSV file
     df = pd.read_csv(csv_file)
     first_names = df['FirstName'].tolist()
 
-    # Temporary directory to save personalized videos
     if not os.path.exists('temp_videos'):
         os.makedirs('temp_videos')
-    
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix='.mp4') as temp_video:
+        temp_video.write(video_file.getvalue())
+        temp_video_path = temp_video.name
+
     with st.spinner('Processing videos... Please wait.'):
         for name in first_names:
-            # Load the video file
-            clip = VideoFileClip(video_file.name)
-            
-            # Create a text clip
+            clip = VideoFileClip(temp_video_path)
             txt_clip = TextClip(f"{name}, please enroll at ABC College", fontsize=24, color='white')
             txt_clip = txt_clip.set_position('center').set_duration(10)
-            
-            # Overlay the text on the original video clip
             video = CompositeVideoClip([clip, txt_clip])
-            
-            # Output file name
             output_filename = f"temp_videos/personalized_{name}.mp4"
-            
-            # Write the result to a file
             video.write_videofile(output_filename, codec="libx264", fps=24)
 
     st.success('Videos processed successfully!')
 
-    # Provide download links
     for name in first_names:
         output_filename = f"temp_videos/personalized_{name}.mp4"
         with open(output_filename, "rb") as file:
