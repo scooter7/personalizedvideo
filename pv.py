30cd829fcec67c06a16c71bf5d82445c12421add
scooter7 <scooter.vineburgh@gmail.com>
2024-04-06 13:43:42 -0500

    Update pv.py

--- pv.py
+++ pv.py
@@ -8,10 +8,20 @@ from moviepy.editor import AudioFileClip, VideoFileClip
 
 st.title("Video Personalization App")
 
+# Initialize session state variables if they don't exist
+if 'processing_done' not in st.session_state:
+    st.session_state.processing_done = False
+
 video_file = st.file_uploader("Choose a video file", type=["mp4"])
 csv_file = st.file_uploader("Choose a CSV file")
 
-if video_file and csv_file:
+# Button to start processing
+start_processing = st.button('Start Processing')
+
+if start_processing:
+    st.session_state.processing_done = False  # Reset the flag when button is pressed
+
+if video_file and csv_file and not st.session_state.processing_done:
     df = pd.read_csv(csv_file)
     first_names = df['FirstName'].tolist()
 
@@ -36,7 +46,6 @@ if video_file and csv_file:
             if not ret:
                 break
 
-            # Define font, text, position, and add text
             font = cv2.FONT_HERSHEY_SIMPLEX
             text = f"{name}, please enroll at ABC College"
             text_position = (50, height - 50)
@@ -45,7 +54,6 @@ if video_file and csv_file:
             line_type = 2
 
             cv2.putText(frame, text, text_position, font, font_scale, font_color, line_type)
-
             out.write(frame)
 
         cap.release()
@@ -63,3 +71,4 @@ if video_file and csv_file:
         os.remove(final_output_filename)
 
     os.unlink(video_path)
+    st.session_state.processing_done = True  # Set flag to True after processing
